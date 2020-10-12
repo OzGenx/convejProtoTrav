1897d18c3e9611f5ade8fee874de57fa6d3de60a
GenX <hybridgenerationx@gmail.com>
2020-10-12 18:13:38 +0200

    commitbckp

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// bring in expess, using common js module not es6
+const express = require('express')
+const dotenv = require('dotenv')
+const products = require('./data/products')
+
+dotenv.config()
+
+const app = express()
+
+//If we get a request for '/' then we running a function that takes in a request and response object
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
