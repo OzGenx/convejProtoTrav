51a708bf2373366af37e20033f8724ad3ac24aab
GenX <hybridgenerationx@gmail.com>
2020-11-26 20:51:32 +0200

    frontend build before deployment

---
+++ frontend/build/precache-manifest.d8b03d3956963cf073803648c296882f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5c5ffa40f26ebaf346bd0761a19aa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff63710a7bab8bcc345",
+    "url": "/static/css/main.9c24d21d.chunk.css"
+  },
+  {
+    "revision": "24594ebdbd2fe841c3eb",
+    "url": "/static/js/2.5be8206c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5be8206c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff63710a7bab8bcc345",
+    "url": "/static/js/main.bf7045af.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
